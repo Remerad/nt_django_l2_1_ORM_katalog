74dd2ca88c4a4cd5cfbaaf3a70cf1ad99b118707
Remerad <jgodgiven@gmail.com>
2022-02-10 18:22:51 +0300

    Нет отображения товаров в браузере.

--- phones/views.py
+++ phones/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from phones.models import Phone
@@ -7,6 +8,7 @@ def show_catalog(request):
     template = 'catalog.html'
 
     phones = Phone.objects.all().order_by('release_date')
+    print(phones)
     sort = request.GET.get("sort", "")
     if sort == 'name':
         phones = Phone.objects.order_by('name')
@@ -18,10 +20,15 @@ def show_catalog(request):
     context = {
         'catalog': phones,
          }
+    #return HttpResponse(phones)
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     context = {}
+    phone = Phone.objects.filter(slug=slug)
+    context = {
+        'phone': phone,
+    }
     return render(request, template, context)
